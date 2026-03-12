9311b52d7ec9d8d92302aef28d2650718d3b7c51
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-03-12 20:50:59 +0200

    Refactor Lesson_2.py by reordering code blocks.

--- Lesson_2.py
+++ Lesson_2.py
@@ -1,12 +1,11 @@
 from helper_functions import print_llm_response, get_llm_response
 
+
 ## ------------------------------------------------------ ##
-list_of_tasks = [
-                "Compose a brief email to my boss explaining that I will be late for tomorrow's "
+list_of_tasks = ["Compose a brief email to my boss explaining that I will be late for tomorrow's "
                 "meeting.",
                 "Write a birthday poem for Otto, celebrating his 28th birthday.",
-                "Write a 300-word review of the movie 'The Arrival'."
-                ]
+                "Write a 300-word review of the movie 'The Arrival'."]
 
 print(list_of_tasks)
 
