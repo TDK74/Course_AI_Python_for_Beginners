3b712d62afc848c131150051521a0f13b2dce793
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-03-14 23:00:52 +0200

    Python file Lesson_7.py added.

---
+++ Lesson_7.py
@@ -0,0 +1,21 @@
+from helper_functions import *
+
+
+## ------------------------------------------------------ ##
+journal = read_journal("journal_entries/cape_town.txt")
+print(journal)
+
+## ------------------------------------------------------ ##
+prompt = f"""
+        Given the following journal entry from a food critic, identify the restaurants and
+         their specialties.
+        For each restaurant, highlight its name and specialties in bold and use different
+         colors for each.
+        Provide the output as HTML suitable for display in a Jupyter notebook.
+
+        Journal entry:
+        {journal}
+        """
+
+html_response = get_llm_response(prompt)
+display_html(html_response)
