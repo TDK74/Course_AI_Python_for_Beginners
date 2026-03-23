5250ec59ecca93f2f467a73609634877adfce450
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-03-23 11:28:38 +0200

    Python file Lesson_4.py added.

---
+++ Lesson_4.py
@@ -0,0 +1,36 @@
+print("Hello, World!")
+
+## ------------------------------------------------------ ##
+print("Hello to me. My name is Tsetso.")
+
+## ------------------------------------------------------ ##
+print("Hello, my best friend and my girlfriend, and my pet!")
+
+## ------------------------------------------------------ ##
+print("This is a print statement!")
+
+## ------------------------------------------------------ ##
+print("Hello, Tsetso!")
+print("How is your day going?")
+
+## ------------------------------------------------------ ##
+# intended mistake
+print("Hello, Andrew!)
+
+## ------------------------------------------------------ ##
+print("Hello, Andrew!")
+
+## ------------------------------------------------------ ##
+print('My favorite colour is - blue.')
+
+## ------------------------------------------------------ ##
+print('How are you feeling today, Tsetso?')
+print("I am feeling great. Thanks! And you?")
+
+## ------------------------------------------------------ ##
+# intended mistake
+print('How are you feeling today, Tsetso?')
+print("I am feeling great. Thanks! And you?"
+
+## ------------------------------------------------------ ##
+print("I am feeling great. Thanks! And you?")
