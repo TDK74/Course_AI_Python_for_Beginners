c870822cc677c675ef7651ee01c85e17ec3f3cc8
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2026-03-26 21:25:16 +0200

    Python file Lesson_8.py added.

---
+++ Lesson_8.py
@@ -0,0 +1,104 @@
+age = 28
+
+## ------------------------------------------------------ ##
+print(age)
+
+## ------------------------------------------------------ ##
+age = 5
+
+## ------------------------------------------------------ ##
+print(age)
+
+## ------------------------------------------------------ ##
+name = "Otto"
+gnome_height = 12.7
+
+## ------------------------------------------------------ ##
+print(f"Age: {age}")
+
+## ------------------------------------------------------ ##
+print(f"Name: {name}")
+print(f"Gnome height: {gnome_height}")
+
+## ------------------------------------------------------ ##
+# intended mistake
+print(f"Gnome height: {Gnome_height}")
+
+## ------------------------------------------------------ ##
+score = 0
+print(score)
+
+## ------------------------------------------------------ ##
+score = score + 50
+print(score)
+
+## ------------------------------------------------------ ##
+score = score + 100
+print(score)
+
+## ------------------------------------------------------ ##
+score = score + 300
+print(score)
+
+## ------------------------------------------------------ ##
+print(f"Your final score was: {score}")
+
+## ------------------------------------------------------ ##
+# intended mistake
+my score = 450
+
+## ------------------------------------------------------ ##
+print(49 / 7)
+
+## ------------------------------------------------------ ##
+print(f"Otto's age in dog years is {49 / 7}")
+
+## ------------------------------------------------------ ##
+dog_age = 49 / 7
+
+## ------------------------------------------------------ ##
+print(f"""Otto's dog age is {49 / 7}. So a dog that's about
+    {49 / 7} would be the same age as Otto. Any dog born about {49 / 7}
+    years ago would be in the same stage of life as Otto.""")
+
+## ------------------------------------------------------ ##
+print(f"""Otto's dog age is {dog_age}. So a dog that's about
+    {dog_age} would be the same age as Otto. Any dog born about {dog_age}
+    years ago would be in the same stage of life as Otto.""")
+
+## ------------------------------------------------------ ##
+dog_age = 50 / 7
+
+## ------------------------------------------------------ ##
+print(f"""Otto's dog age is {dog_age}. So a dog that's about
+    {dog_age} would be the same age as Otto. Any dog born about {dog_age}
+    years ago would be in the same stage of life as Otto.""")
+
+## ------------------------------------------------------ ##
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+    {dog_age} would be the same age as {name}. Any dog born about {dog_age}
+    years ago would be in the same stage of life as {name}.""")
+
+## ------------------------------------------------------ ##
+name = "Otto Matic"
+
+## ------------------------------------------------------ ##
+print(f"""{name}'s dog age is {dog_age}. So a dog that's about
+    {dog_age} would be the same age as {name}. Any dog born about {dog_age}
+    years ago would be in the same stage of life as {name}.""")
+
+## ------------------------------------------------------ ##
+my_name = 'Tsetso'
+print(f"Hi, {my_name}")
+
+## ------------------------------------------------------ ##
+fav_num = 101
+print(f"Your favorite number plus 10 is {fav_num + 10}")
+
+## ------------------------------------------------------ ##
+countries_visited = 12
+countries_to_visit = 22
+
+print(f"""I have visited {countries_visited} countries. I plan to visit {countries_to_visit} more
+     countries, and when I'm done I will have visited {countries_visited + countries_to_visit}
+     countries.""")
